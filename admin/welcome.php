65611d57b18239cd2348e62fa0e5438c00efce30
Harshithasr1 <109043245+Harshithasr1@users.noreply.github.com>
2023-07-03 15:51:30 -0500

    css update

---
+++ admin/welcome.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>	
+	<style>
+		body{			
+			background-image: url(../customer/partials/image5.jpg);		
+			background-size: cover;
+			background-repeat: no-repeat;
+			position: absolute;
+		}
+	</style>
+
+    <meta charset="UTF-8">     
+    <link rel="stylesheet" href="../css/welcome.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+	<section class="header">
+	<nav>		
+	<div class= 'navbar'> 		
+		<ul>
+			<b><i class="fas fa-pizza-slice" style="font-size:48px;color:blue"></i> PIZZERIA </b>			
+			<li><a href="welcome.php"><i class="fa fa-fw fa-home"></i> Home</a><li>
+			<li><a href="back.php"><i class="fa fa-fw fa-user"></i> Login/Signup</a><li>
+		</ul>		
+	</div>
+	</nav>
+	</section>
+    <div class = "text-box">				       
+		<p style="margin-top: 12%; margin-left: -15%; color:brown; font-size:350%; font-family:pristina">WELCOME TO PIZZERIA!!</p> 
+		<p style="margin-left: -15%; color:black; font-size:180%; font-family:verdana; font-weight: bold">CREATE YOUR OWN PIZZA</p>    
+		<p style="margin-left: -15%; color:black; font-size:150%; font-family:papyrus; font-weight: bold">Place your order online and enjoy a wide variety of pizzas at your comfort...</p>		
+    </div>
+
+<div class = "footer">		
+	<p>Copyright &copy;2021 All rights reserved, Pizzeria.</p>		
+</div>
+</body>
+</html>
--- customer/partials/footer.php
+++ customer/partials/footer.php
@@ -1,5 +1,5 @@
 <div class = "footer">		
 	<p>Copyright &copy;2021 All rights reserved, Pizzeria.</p>		
 </div>
-
+</body>
 </html>
